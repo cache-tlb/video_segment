a39a3c0e71ba1345290193c2e8219c8cac62677b
cache-tlb <liubin1991@outlook.com>
2016-01-21 13:50:56 +0800

    fix interface error in api.h

--- api.h
+++ api.h
@@ -4,6 +4,6 @@
 #include <opencv2/opencv.hpp>
 #include <vector>
 
-bool GraphBasedHierarchicalSegmentation(const std::vector<cv::Mat> &frames, float c, float c_reg, int min_size, float sigma, int hie_num);
+bool GraphBasedHierarchicalSegmentation(const std::vector<cv::Mat> &frames, float c, float c_reg, int min_size, float sigma, int hie_num, int output_level, std::vector<cv::Mat> &output_frame_labels);
 
 #endif
